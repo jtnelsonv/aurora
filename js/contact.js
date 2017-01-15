670773368ae7d522d59be82f3921e4775cddc1ef
Joshua <jtnelson@protonmail.com>
2017-01-15 13:24:49 -0700

    Form Test
    Testing still needed to be done on contact form.

--- js/contact.js
+++ js/contact.js
@@ -1,50 +1,51 @@
-$("#contactForm").submit(function(event) {
-  if (even.isDefaultPrevented()) {
-    formError();
-    formStatus(false, "The form is not filled out properly.");
-  } else {
-      event.preventDefault();
-      submitForm();
-  }
+$("#contactForm").validator().on("submit", function(event) {
+    if (event.isDefaultPrevented()) {
+        formError();
+        formStatus(false, "The form is not filled out properly.");
+    } else {
+        event.preventDefault();
+        submitForm();
+    }
 });
 
 function submitForm() {
-  //Initiate variables with form content
-  var name = $("#name").val();
-  var email = $("#email").val();
-  var message = $("#message").val();
+    //Initiate variables with form content
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
 
-//AJAX
-  $.ajax( {
-    type: "POST",
-    url: "../php/process.php",
-    data: "name=" + name + "&email=" + email + "&message=" + message,
-    success : function(text) {
-      if (text == "success") {
-        formSuccess();
-      } else {
-        formError();
-        formStatus(false,text);
-      }
-    }
-  });
+    //AJAX
+    $.ajax({
+        type: "POST",
+        url: "../php/process.php",
+        data: "name=" + name + "&email=" + email + "&message=" + message,
+        success: function(text) {
+            if (text == "success") {
+                formSuccess();
+            } else {
+                formError();
+                formStatus(false, text);
+            }
+        }
+    });
 }
 
 function formSuccess() {
-  $("#contactForm")[0].reset();
-  formStatus(true, "Message sent!");
+    $("#contactForm")[0].reset();
+    formStatus(true, "Message sent!");
 }
 
 function formError() {
-  $("#contactForm").removeClass().addClass('text-danger');
+    $("#contactForm").removeClass().addClass('shake animated').one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+        $(this).removeClass();
+    });
 }
 
 function formStatus(valid, msg) {
-  var msgClass;
-  if(valid) {
-    msgClass = "text-success";
-  } else {
-    msgClass = "text-danger";
-  }
-  $("#formStatus").removeClass().addClass(msgClass).text(msg);
+    if (valid) {
+        msgClass = "h3 text-center tada animated text-success";
+    } else {
+        msgClass = "h3 text-center text-danger";
+    }
+    $("#formStatus").removeClass().addClass(msgClass).text(msg);
 }
--- php/process.php
+++ php/process.php
@@ -1,55 +1,93 @@
 <?php
 $errorMsg = ""; //Variable for error messages
 
-//Name
-if(empty($_POST["name"])) {
-	$errorMsg = "Your name is required.";
-} else {
-	$name = $_POST["name"];
-}
+// Checks server request method is post
 
-//Email
-if(empty($_POST["email"])) {
-	$errorMsg = "A valid email is required.";
-} else {
-	$email = $_POST["email"];
-}
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
-//Message
-if(empty($_POST["message"])) {
-	$errorMsg = "Please include a message.";
-} else {
-	$message = $_POST["message"];
-}
+	// Name
 
-//Email info and formatting
-	$emailTo = "contact@joshuanelson.me";
-	$subject = "New Contact Form Message";
+	if (empty($_POST["name"])) {
+		$errorMsg = "Your name is required.";
+	}
+	else {
+		$name = validate($_POST["name"]);
 
-	$body .= "Name: ";
-	$body .= $name;
-	$body .= "\n";
+		// Checks to make sure name contains only letters
 
-	$body .= "Email: ";
-	$body .= $email;
-	$body .= "\n";
-
-	$body .= "Message: ";
-	$body .= $message;
-	$body .= "\n";
-
-	//Send email
-	$success = mail($emailTo, $subject, $body, "From:".$email);
-
-	//Redirect to success page
-	if ($success && $errorMsg == "") {
-		echo "success";
-	} else {
-		if($errorMsg == "") {
-			echo "Something didn't work quite right.";
-		} else {
-			echo $errorMsg;
+		if (!preg_match("/^[a-zA-Z]*$/", $name)) {
+			$errorMsg = "Name may only contain letters.";
 		}
 	}
 
- ?>
+	// Email
+
+	if (empty($_POST["email"])) {
+		$errorMsg.= "A valid email is required.";
+	}
+	else {
+		$email = validate($_POST["email"]);
+
+		// Checks email format
+
+		if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+			$errorMsg = "Invalid email format.";
+		}
+	}
+
+	// Message
+
+	if (empty($_POST["message"])) {
+		$errorMsg.= "Please include a message.";
+	}
+	else {
+		$message = validate($_POST["message"]);
+		if (!preg_match("/^[a-zA-Z0-9]*$/", $message)) {
+			$errorMsg = "Your message may only contain letters and numbers.";
+		}
+	}
+
+	// Email info and formatting
+
+	$emailTo = "contact@joshuanelson.me";
+	$subject = "New Contact Form Message";
+	$body.= "Name: ";
+	$body.= $name;
+	$body.= "\n";
+	$body.= "Email: ";
+	$body.= $email;
+	$body.= "\n";
+	$body.= "Message: ";
+	$body.= $message;
+	$body.= "\n";
+
+	// Send email
+
+	$success = mail($emailTo, $subject, $body, "From:" . $email);
+
+	// Redirect to success page
+
+	if ($success && errorMsg == "") {
+		echo "success";
+	}
+	else {
+		if ($errorMsg == "") {
+			echo "Something didn't work quite right.";
+		}
+		else {
+			echo $errorMsg;
+		}
+	}
+}
+
+// Function to validate form fields
+
+function validate($data)
+{
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = htmlspecialchars($data);
+	return $data;
+}
+
+?>
